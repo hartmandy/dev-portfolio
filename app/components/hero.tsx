d6354b4d6bcc952bbaba62c07bc10dc4cd6de631
hartmandy <120344775+hartmandy@users.noreply.github.com>
2025-06-01 12:34:19 -0400

    social links

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,3 +1,9 @@
+import {
+  EnvelopeClosedIcon,
+  GitHubLogoIcon,
+  LinkedInLogoIcon,
+} from "@radix-ui/react-icons";
+
 export function Hero() {
   const text =
     "Hello, I'm Mandy. Front End Engineer currently working at Sports Card Investor.";
@@ -11,8 +17,8 @@ export function Hero() {
   ];
 
   return (
-    <div className="mt-[100px] ml-10 md:max-w-xl">
-      <h1 className="text-6xl font-bold whitespace-pre-wrap">
+    <div className="mt-[100px] ml-10 max-w-xs md:max-w-2xl">
+      <h1 className="text-3xl md:text-6xl font-bold whitespace-pre-wrap">
         {text.split(" ").map((word, wordIndex) => (
           <span key={wordIndex} className="inline-block mr-2">
             {word.split("").map((char, charIndex) => (
@@ -28,6 +34,17 @@ export function Hero() {
           </span>
         ))}
       </h1>
+      <div className="flex justify-start gap-3 md:w-2/5 mt-6">
+        <a href="https://github.com/hartmandy" className="h-8 w-8">
+          <GitHubLogoIcon className="h-full w-full text-neutral-700 transition-all duration-1000 hover:duration-100 ease-in-out hover:text-sky-800" />
+        </a>
+        <a href="https://linkedin.com/in/hartmandy" className="h-8 w-8">
+          <LinkedInLogoIcon className="h-full w-full text-neutral-700 transition-all duration-1000 hover:duration-100 ease-in-out hover:text-sky-600" />
+        </a>
+        <a href="mailto:mandyhartmandev@gmail.com" className="h-8 w-8">
+          <EnvelopeClosedIcon className="h-full w-full text-neutral-700 transition-all duration-1000 hover:duration-100 ease-in-out hover:text-sky-400" />
+        </a>
+      </div>
     </div>
   );
 }
